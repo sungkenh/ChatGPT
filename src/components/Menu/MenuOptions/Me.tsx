12cee303ad976f12ff45468d298f500514d3b98f
sungkenh <sungkenh@gmail.com>
2023-04-06 15:21:59 +0900

    Update Me.tsx

--- src/components/Menu/MenuOptions/Me.tsx
+++ src/components/Menu/MenuOptions/Me.tsx
@@ -8,11 +8,11 @@ const Me = () => {
   return (
     <a
       className='flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-      href='https://github.com/pbk123461/ChatGPT'
+      href=''
       target='_blank'
     >
       <HeartIcon />
-      {t('Made by Jing Hua. This website is made by pbk123461')}
+      {t('')}
     </a>
   );
 };
